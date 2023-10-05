619ad060b0334db2824b33546f86533f7c24c84f
bboysquiz <mrkiraller@gmail.com>
2023-10-05 14:42:09 +0300

    added pagination and search input

--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -1,13 +1,54 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed, Ref } from 'vue';
 import userData from '../data'
+import { User, PageAction } from '../types';
 
 
-const users = ref(userData);
+const users: User[] = userData;
+const currentPage: Ref<number> = ref(1);
+const search: Ref<string> = ref('');
+const perPage: number = 5;
+const startPage: Ref<number> = ref(0);
+const endPage: Ref<number> = ref(5);
+
+
+const filteredUsers = computed(() => {
+    return users.filter(user => user.name.toLowerCase().includes(search.value.toLowerCase()))
+})
+
+const totalPages = computed(() => {
+    return Math.ceil(filteredUsers.value.length / perPage);
+});
+
+const pageNumbers = computed(() => {
+    return Array.from({ length: totalPages.value }, (_, index) => index + 1);
+});
+
+const displayedUsers = computed(() => {
+    startPage.value = (currentPage.value - 1) * perPage;
+    endPage.value = startPage.value + perPage;
+    return filteredUsers.value.slice(startPage.value, endPage.value);
+});
+
+const gotoPage = (pageNumber: number) => {
+    currentPage.value = pageNumber
+};
+const changePage = (action: PageAction) => {
+    if (action === 'prev'){
+        currentPage.value--
+    }else if(action === 'next'){
+        currentPage.value++
+    }
+}
+
+const handleSearchChange = () => {
+    currentPage.value = 1;
+}
 
 </script>
 
 <template>
+    <input type="text" v-model="search" @input="handleSearchChange">
     <table>
         <thead>
             <tr>
@@ -19,7 +60,7 @@ const users = ref(userData);
             </tr>
         </thead>
         <tbody>
-            <tr v-for="user in users" :key="user.id">
+            <tr v-for="user in displayedUsers" :key="user.id">
                 <td>{{ user.id }}</td>
                 <td>{{ user.name }}</td>
                 <td>{{ user.age }}</td>
@@ -31,6 +72,29 @@ const users = ref(userData);
             </tr>
         </tbody>
     </table>
+    <div class="pagination">
+        <button @click="changePage('prev')" :disabled="currentPage === 1">Prev</button>
+        <button class="pagination-el" v-for="item in pageNumbers" :key="item" @click="gotoPage(item)">{{ item }}</button>
+        <button @click="changePage('next')" :disabled="currentPage === totalPages">Next</button>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.pagination {
+    display: flex;
+    width: 500px;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.pagination-el {
+    cursor: pointer;
+    background-color: grey;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+}
+</style>
