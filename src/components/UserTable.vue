9ccb408785e61186719cd45066e972ebf8c27357
bboysquiz <mrkiraller@gmail.com>
2023-10-05 15:25:41 +0300

    added sorting by age and some styles

--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -10,12 +10,18 @@ const search: Ref<string> = ref('');
 const perPage: number = 5;
 const startPage: Ref<number> = ref(0);
 const endPage: Ref<number> = ref(5);
+const sortAscending: Ref<boolean> = ref(true);
 
 
 const filteredUsers = computed(() => {
-    return users.filter(user => user.name.toLowerCase().includes(search.value.toLowerCase()))
+    let result = users.filter(user => user.name.toLowerCase().includes(search.value.toLowerCase()));
+    if (!sortAscending.value) {
+        result = result.slice().sort((a, b) => b.age - a.age);
+    } else {
+        result = result.slice().sort((a, b) => a.age - b.age);
+    }
+    return result
 })
-
 const totalPages = computed(() => {
     return Math.ceil(filteredUsers.value.length / perPage);
 });
@@ -34,9 +40,9 @@ const gotoPage = (pageNumber: number) => {
     currentPage.value = pageNumber
 };
 const changePage = (action: PageAction) => {
-    if (action === 'prev'){
+    if (action === 'prev') {
         currentPage.value--
-    }else if(action === 'next'){
+    } else if (action === 'next') {
         currentPage.value++
     }
 }
@@ -45,37 +51,51 @@ const handleSearchChange = () => {
     currentPage.value = 1;
 }
 
+const sortByAge = () => {
+    sortAscending.value = !sortAscending.value;
+}
+
 </script>
 
 <template>
-    <input type="text" v-model="search" @input="handleSearchChange">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Last Login</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="user in displayedUsers" :key="user.id">
-                <td>{{ user.id }}</td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.age }}</td>
-                <td>{{ user.last_login }}</td>
-                <td>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <div class="pagination">
-        <button @click="changePage('prev')" :disabled="currentPage === 1">Prev</button>
-        <button class="pagination-el" v-for="item in pageNumbers" :key="item" @click="gotoPage(item)">{{ item }}</button>
-        <button @click="changePage('next')" :disabled="currentPage === totalPages">Next</button>
+    <div class="content">
+        <div class="header">
+            <input class="search" type="text" v-model="search" @input="handleSearchChange">
+            <button class="sort-button" @click="sortByAge">
+                sort by age 
+                <img class='sort-arrow' src="../assets/arrow.png" :style="{ transform: sortAscending ? 'rotateZ(-90deg)' : 'rotateZ(90deg)' }" alt="arrow">
+            </button>
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Last Login</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in displayedUsers" :key="user.id">
+                    <td>{{ user.id }}</td>
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.age }}</td>
+                    <td>{{ user.last_login }}</td>
+                    <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="pagination">
+            <button @click="changePage('prev')" :disabled="currentPage === 1">Prev</button>
+            <button class="pagination-el" v-for="item in pageNumbers" :key="item" @click="gotoPage(item)">{{ item
+            }}</button>
+            <button @click="changePage('next')" :disabled="currentPage === totalPages">Next</button>
+        </div>
     </div>
 </template>
 
@@ -96,5 +116,41 @@ const handleSearchChange = () => {
     align-items: center;
     justify-content: center;
     border-radius: 10px;
+}
+
+.sort-arrow {
+    width: 25px;
+    height: 25px;
+}
+
+.sort-button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+}
+
+.search {
+    width: 300px;
+}
+
+.table {
+    width: 1000px;
+}
+
+.table td {
+    width: 200px;
+}
+.content {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 1000px;
 }
 </style>
