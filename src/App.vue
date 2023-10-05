c8fa39be87e1b21670cfe5f80a5c0ad9065b1130
bboysquiz <mrkiraller@gmail.com>
2023-10-05 11:02:30 +0300

    created table with data from another file, put data into a separate file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+import UserTable from './components/UserTable.vue';
 </script>
 
 <template>
- 
+ <UserTable />
 </template>
 
 <style scoped>
--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import userData from '../data'
+
+
+const users = ref(userData);
 
 </script>
 
-<template></template>
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Last Login</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.age }}</td>
+                <td>{{ user.last_login }}</td>
+                <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
 
 <style scoped></style>
